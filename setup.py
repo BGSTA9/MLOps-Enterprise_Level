ec32bcc2764380aad215c3cdfe04c27212c18f9d
BGSTA9 <soheilsanatii@gmail.com>
2025-07-07 17:59:59 +0300

    "requirements.txt" added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MLOPs_Uno",
+    version="0.0.0",
+    author="Soheil Sanati",
+    author_email="soheilsanatii@gmail.com",
+    packages=find_packages()
+)
